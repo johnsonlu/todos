6b312ce394aa5c427ebd163c6112c727e1853a80
Lu Jianxin <johnsonlu@outlook.com>
2018-08-02 23:06:34 +0800

    Add reducers.

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import {
+    combineReducers
+} from "redux";
+import todos from "./todos";
+import visiblityFilter from "./visiblityFilter";
+
+export default combineReducers({
+    todos,
+    visiblityFilter
+});
